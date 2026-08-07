97ae0aa7f28fcc74c46a5e4b55635c415768334c
hu19 <hu19@jrlogin10.jureca>
2026-08-07 15:10:17 +0200

    make badwords filter into a class

--- src/llm_data/filters/c4/badwords.py
+++ src/llm_data/filters/c4/badwords.py
@@ -1,7 +1,13 @@
 import daft
 import requests
 
+from daft import DataFrame, col
 from llm_data.config import BAD_WORDS_URL
+
+
+@daft.func
+def has_badword(text: str, badwords: set) -> bool:
+    return not set(text.lower().split()).isdisjoint(badwords)
 
 
 def load_badwords(url: str = BAD_WORDS_URL) -> frozenset[str]:
@@ -10,10 +16,23 @@ def load_badwords(url: str = BAD_WORDS_URL) -> frozenset[str]:
     return frozenset(w.strip().lower() for w in resp.text.splitlines() if w.strip())
 
 
-def make_badwords_filter(badwords: frozenset[str]):
+class BadWordsFilter:
 
-    @daft.func
-    def has_badword(text: str) -> bool:
-        return not set(text.lower().split()).isdisjoint(badwords)
+    def __init__(
+        self,
+        input_column: str = "text",
+        badwords: set[str] | None = None,
+        badwords_url: str =BAD_WORDS_URL,
+        name: str = "BadWordsFilter",
+    ):
+        self.input_column = input_column
+        self.name = name
+    
+        if badwords is not None:
+            self.badwords = badwords
+        else:
+            self.badwords = load_badwords(badwords_url)
 
-    return has_badword
+    def __call__(self, df: DataFrame) -> DataFrame:
+        df = df.where(~has_badword(col(self.input_column), self.badwords))
+        return df
